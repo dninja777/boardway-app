a8c1d29be8ba9d6ae826059d22167c7b36e73043
dninja777 <duroneppsjr@hotmail.com>
2017-01-25 02:27:57 +0000

    Add average review rating and authenticate_user

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,6 +1,7 @@
 class PlaysController < ApplicationController
     
     before_action :find_play, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:new, :edit]
     
     def index
         if params[:category].blank?
@@ -8,11 +9,15 @@ class PlaysController < ApplicationController
     else
         @category_id = Category.find_by(name: params[:category]).id
         @plays = Play.where(:category_id => @category_id).order("created_at DESC")
-    end
+        end
     end
     
     def show
-      
+      if @play.reviews.blank?
+          @average_review = 0
+      else
+          @average_review = @play.reviews.average(:rating).round(2)
+      end
     end
     
     def new
